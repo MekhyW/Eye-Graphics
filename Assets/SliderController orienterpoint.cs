3ebb30aea8931433ec465eda2ba37901e847c89d
Felipe Catapano Emrich Melo <mefkhytheroboticist@gmail.com>
2023-08-31 14:01:08 -0300

    mask_example_static

--- Assets/SliderController orienterpoint.cs
+++ Assets/SliderController orienterpoint.cs
@@ -6,11 +6,15 @@ public class SliderController : MonoBehaviour
     public Slider xSlider;
     public Slider ySlider;
     public Transform objectToMove;
+    public float offsetX = 0;
+    public float offsetY = 0;
 
     private void Update()
     {
         float xValue = xSlider.value;
         float yValue = ySlider.value;
+        xValue += offsetX;
+        yValue += offsetY;
         objectToMove.position = new Vector3(xValue, yValue, objectToMove.position.z);
     }
 }
